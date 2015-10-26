fd892a716653aa450c70bd9912fec06ca32c3fc0
Alexandre Dutra <alexandre.dutra@datastax.com>
2015-10-26 18:15:40 +0100

    JAVA-846: Codec support for Joda Time.
    This commit introduces new codecs to automatically map Joda Time types such
    as LocalTime, LocalDate, Instant and DateTime to CQL types (respectively, time, date,
    timestamp and tuple<timestamp,varchar>).

---
+++ driver-extras/src/test/java/com/datastax/driver/extras/codecs/joda/LocalDateCodecTest.java
@@ -0,0 +1,58 @@
+/*
+ *      Copyright (C) 2012-2015 DataStax Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.datastax.driver.extras.codecs.joda;
+
+import org.joda.time.LocalDate;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LocalDateCodecTest {
+
+    private static final LocalDate EPOCH = new LocalDate(0);
+    private static final LocalDate MIN = LocalDate.parse("-5877641-06-23");
+
+    @DataProvider(name = "LocalDateCodecTest")
+    public Object[][] parseParameters() {
+        return new Object[][]{
+            { "0"                 , MIN },
+            { "'2147483648'"      , EPOCH },
+            { "'-5877641-06-23'"  , MIN },
+            { "'1970-01-01'"      , EPOCH },
+            { "'2014-01-01'"      , LocalDate.parse("2014-01-01") }
+        };
+    }
+
+    @Test(groups = "unit", dataProvider = "LocalDateCodecTest")
+    public void should_parse_valid_formats(String input, LocalDate expected) {
+        // when
+        LocalDate actual = LocalDateCodec.instance.parse(input);
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test(groups = "unit")
+    public void should_format_valid_object() {
+        // given
+        LocalDate input = LocalDate.parse("2010-06-30");
+        // when
+        String actual = LocalDateCodec.instance.format(input);
+        // then
+        assertThat(actual).isEqualTo("'2010-06-30'");
+    }
+
+}
