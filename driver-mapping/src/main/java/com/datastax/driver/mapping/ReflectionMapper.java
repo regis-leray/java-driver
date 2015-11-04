7e13ee35a287fa78620c76dba67275d5bb5e9234
Alexandre Dutra <alexandre.dutra@datastax.com>
2015-11-04 16:11:04 +0100

    JAVA-605: Codec support for Java enums.
    This commit does further code cleanup: delete unused methods and types.

--- driver-mapping/src/main/java/com/datastax/driver/mapping/ReflectionMapper.java
+++ driver-mapping/src/main/java/com/datastax/driver/mapping/ReflectionMapper.java
@@ -19,11 +19,7 @@ import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
-
-import com.google.common.reflect.TypeToken;
 
 import com.datastax.driver.core.ConsistencyLevel;
 
@@ -83,58 +79,6 @@ class ReflectionMapper<T> extends EntityMapper<T> {
             } catch (Exception e) {
                 throw new IllegalStateException("Unable to access setter for '" + fieldName + "' in " + entity.getClass().getName(), e);
             }
-        }
-    }
-
-    private static class EnumMapper<T> extends LiteralMapper<T> {
-
-        private final EnumType enumType;
-        private final Map<String, Object> fromString;
-
-        private EnumMapper(Field field, int position, PropertyDescriptor pd, EnumType enumType, AtomicInteger columnCounter) {
-            super(field, position, pd, columnCounter);
-            this.enumType = enumType;
-
-            if (enumType == EnumType.STRING) {
-                fromString = new HashMap<String, Object>(fieldType.getRawType().getEnumConstants().length);
-                for (Object constant : fieldType.getRawType().getEnumConstants())
-                    fromString.put(constant.toString().toLowerCase(), constant);
-
-            } else {
-                fromString = null;
-            }
-        }
-
-        @SuppressWarnings("rawtypes")
-        @Override
-        public Object getValue(T entity) {
-            Object value = super.getValue(entity);
-            switch (enumType) {
-                case STRING:
-                    return (value == null) ? null : value.toString();
-                case ORDINAL:
-                    return (value == null) ? null : ((Enum)value).ordinal();
-            }
-            throw new AssertionError();
-        }
-
-        @Override
-        public void setValue(Object entity, Object value) {
-            Object converted = null;
-            switch (enumType) {
-                case STRING:
-                    converted = fromString.get(value.toString().toLowerCase());
-                    break;
-                case ORDINAL:
-                    converted = fieldType.getRawType().getEnumConstants()[(Integer)value];
-                    break;
-            }
-            super.setValue(entity, converted);
-        }
-
-        @Override
-        public TypeToken<Object> getPivotType() {
-            return enumType.pivotType;
         }
     }
 
